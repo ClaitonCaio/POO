64d31eda1966bf5187643c56827e74cd118e240d
Claiton Caio Rodrigues <102754355+ClaitonCaio@users.noreply.github.com>
2024-04-29 14:39:36 -0300

    Create Principal.java

---
+++ Aula05/Principal.java
@@ -0,0 +1,17 @@
+
+package poligono;
+
+public class Principal {
+
+     
+    public static void main(String[] args) {
+          //Quadrado
+          Quadrado q1 = new Quadrado(5);
+          
+          System.out.println(q1);
+          
+          q1.setLado(6);
+          
+          System.out.println("Lado do triangulo: " + q1.getLado());
+    }
+}
