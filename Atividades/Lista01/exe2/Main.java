b2313c22bdbd4cb12261bbf2104c21251bedc1f5
Claiton Caio Rodrigues <102754355+ClaitonCaio@users.noreply.github.com>
2024-05-18 20:19:03 -0300

    Create Main.java

---
+++ Atividades/Lista01/exe2/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    Pessoa pessoa = new Pessoa("Claiton", 19, "333.333.333-33");
+    Aluno aluno = new Aluno("Claiton", 19, "333.333.333-33", "Ciencia da Computacao", "POO");
+
+        System.out.println(pessoa);
+        System.out.println(aluno);
+
+        }
+}
